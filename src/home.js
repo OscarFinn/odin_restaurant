b61bd3c5c0055bca348728f335c234be9a0040b3
Oscar Finn <oscarfinn1994@gmail.com>
2024-05-14 15:58:43 +0100

    reordered divs

--- src/home.js
+++ src/home.js
@@ -7,11 +7,12 @@ function makeHome() {
     //Home page will show a welcome message, restaurant about, promo images
     const coverImg = document.createElement('img');
     const infoDiv = document.createElement('div');
-    coverImg.src = Cover;
-    contentDiv.appendChild(coverImg);
-
+    
     infoDiv.innerText = "At The Ghost Kitchen we believe that unfulfilled business is still business. We have a team bound to the Earth, selected by their need to provide fulfilling meals. Together we help them move on to the true afterlife."
     contentDiv.appendChild(infoDiv);
+
+    coverImg.src = Cover;
+    contentDiv.appendChild(coverImg);
 }
 export {
     makeHome,
