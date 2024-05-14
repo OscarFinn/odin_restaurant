9689c6289dab6fe4cf73cb9d2fe1463cca8144c0
Oscar Finn <oscarfinn1994@gmail.com>
2024-05-14 15:57:14 +0100

    added a promo mission statement to home page

--- src/home.js
+++ src/home.js
@@ -3,11 +3,15 @@ import Cover from './images/GhostKitchen01.png';
 const contentDiv = document.querySelector("#content");
 
 function makeHome() {
-    contentDiv.textContent = "HOME PRESSED 2"
+    contentDiv.textContent = ""
     //Home page will show a welcome message, restaurant about, promo images
     const coverImg = document.createElement('img');
+    const infoDiv = document.createElement('div');
     coverImg.src = Cover;
     contentDiv.appendChild(coverImg);
+
+    infoDiv.innerText = "At The Ghost Kitchen we believe that unfulfilled business is still business. We have a team bound to the Earth, selected by their need to provide fulfilling meals. Together we help them move on to the true afterlife."
+    contentDiv.appendChild(infoDiv);
 }
 export {
     makeHome,
